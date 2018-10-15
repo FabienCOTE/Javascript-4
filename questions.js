ce7a1940204c7ff3436f79b53a7221acd34fff73
FabienCOTE <fabien.cote@gmail.com>
2018-10-15 11:06:33 +0200

    Fin exercice

--- questions.js
+++ questions.js
@@ -7,7 +7,7 @@ var tailleString = function (texte) {
     return texte.length;
 }
 var remplaceECar = function (texte) {
-    return texte.replace('e',' ');
+    return texte.replace("e"," ");
 }
 var concatString = function (texte1, texte2) {
     return texte1.concat(texte2);
@@ -25,19 +25,19 @@ var minusculeString = function (texte) {
     return texte.toLowerCase();
 }
 var SupprEspaceString = function (texte) {
-    return texte.slice(1, -1);
+    return texte.trim();
 }
 var IsString = function (texte) {
-    return typeof texte === 'string';
+    return typeof texte === "string";
 }
 var AfficherExtensionString = function (texte) {
-    return texte.split('.')[1];
+    return texte.split(".").pop();
 }
 var NombreEspaceString = function (texte) {
-    return texte.match(/[ ]/g).length;
+    return texte.split(" ").length -1;
 }
 var InverseString = function (texte) {
-    return texte.split('').reverse().join('');
+    return texte.split("").reverse().join("");
 }
 
 /**
@@ -50,11 +50,11 @@ var valeurAbsolue = function (nombre) {
     return Math.abs(nombre);
 }
 var valeurAbsolueArray = function (array) {
-    array.splice(0, 4, Math.abs(array[0]), Math.abs(array[1]), Math.abs(array[2]), Math.abs(array[3]));
-    return array;
+    var result = array.map(Math.abs);
+    return result;
 }
 var sufaceCercle = function (rayon) {
-    return Math.ceil(Math.PI * (rayon * rayon));
+    return Math.round(Math.PI * (rayon * rayon));
 }
 var hypothenuse = function (ab, ac) {
     return Math.hypot(ab, ac);
